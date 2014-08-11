eb7aa174642768ebb106f47693f16eeb5b1704a9
xuwei <xuwei@tbkt.cn>
2014-08-12 07:59:07 +0800

    注册页面

--- src/com/tbkt/student/MainActivity.java
+++ src/com/tbkt/student/MainActivity.java
@@ -1,16 +1,31 @@
 package com.tbkt.student;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ScaleDrawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
-
+	EditText username = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		// 设置用户名输入框图标大小
+		Drawable drawable = getResources().getDrawable(R.drawable.icon_username);
+		drawable.setBounds(0, 0, (int)(drawable.getIntrinsicWidth()*0.5), (int)(drawable.getIntrinsicHeight()*0.5));
+		ScaleDrawable sd = new ScaleDrawable(drawable, 0, 100, 100);
+		username = (EditText)findViewById(R.id.username);
+		username.setCompoundDrawables(sd.getDrawable(), null, null, null);
+		int w = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED); 
+		int h = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED); 
+		username.measure(w, h); 
+		System.out.println(username.getMeasuredHeight());
+		
 	}
 
 	@Override
